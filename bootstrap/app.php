9bcaa396c2a54b9ad0940d15097bfc9f465365e7
KhaledMD4321 <ahmed.khaled.contact@gmail.com>
2026-05-07 20:34:27 +0300

    security: 30-min session timeout + login rate limiting (5/min per IP)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,4 +20,11 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->booted(function () {
+        // حماية صفحة تسجيل الدخول: 5 محاولات كحد أقصى في الدقيقة لكل IP
+        RateLimiter::for('login', function (Request $request) {
+            return Limit::perMinute(5)->by($request->ip());
+        });
+    })
+    ->create();
